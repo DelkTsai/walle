bb36c0344d3983793ad1b697947396c0ee222948
zhentaoo <1904974327@qq.com>
2017-08-09 14:25:12 +0800

    cp webapck-config from https://doc.webpack-china.org/configuration/

---
+++ dist/bundle.js
@@ -0,0 +1,125 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["MyLibrary"] = factory();
+	else
+		root["MyLibrary"] = factory();
+})(this, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/assets/";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+window.walle = {
+    dom: document.querySelector('#app'),
+    data: window.data,
+    regex: /\{\{.+\}\}/,
+    render: function () {
+      // 遍历属性s
+      for (var i = 0; i < this.dom.attributes.length; i++) {
+        var attr = this.dom.attributes[i];
+
+        if (this.regex.test(attr.value)) {
+          attr.value = this.attrMapValue(attr.value, this.data);
+        }
+      }
+
+      // 遍历文本节点
+      if (this.regex.test(this.dom.text.trim())) {
+        this.dom.text = this.attrMapValue(this.dom.text, this.data);
+      }
+
+      // v-if v-show v-for ......
+
+    },
+    attrMapValue: function (str, data) {
+      var res = "__none__";
+      var keys = str.replace("{{", '').replace('}}', '').trim().split('.');
+      keys.forEach(key => {
+        key = key.trim();
+        if (res !== '__none__') {
+          res = res[key]
+        } else {
+          res = window[key]
+        }
+      })
+
+      return res;
+    }
+}
+
+// setTimeout(walle.render, 1000)
+walle.render.call(walle);
+
+
+/***/ })
+/******/ ]);
+});
+//# sourceMappingURL=bundle.js.map
---
+++ webpack.config.js
@@ -0,0 +1,121 @@
+const path = require('path');
+
+module.exports = {
+  entry: "./index.js", // string | object | array
+
+  output: {
+    path: path.resolve(__dirname, "dist"), // string
+
+    filename: "bundle.js", // string
+
+    publicPath: "/assets/", // string
+
+    library: "MyLibrary", // string,
+
+    libraryTarget: "umd", // 通用模块定义
+  },
+
+  module: {
+    rules: [
+      // {
+      //   test: /\.jsx?$/,
+      //   include: [
+      //     path.resolve(__dirname, "app")
+      //   ],
+      //   exclude: [
+      //     path.resolve(__dirname, "app/demo-files")
+      //   ],
+      //   issuer: { test, include, exclude },
+      //   enforce: "pre",
+      //   enforce: "post",
+      //   loader: "babel-loader",
+      //   options: {
+      //     presets: ["es2015"]
+      //   },
+      // },
+      // {
+      //   test: /\.html$/,
+      //   test: "\.html$"
+      //   use: [
+      //     "htmllint-loader",
+      //     {
+      //       loader: "html-loader",
+      //       options: {
+      //       }
+      //     }
+      //   ]
+      // },
+
+    ],
+  },
+
+  resolve: {
+    modules: [
+      "node_modules",
+      path.resolve(__dirname, "app")
+    ],
+    // 用于查找模块的目录
+
+    extensions: [".js", ".json", ".jsx", ".css"],
+    // 使用的扩展名
+
+    alias: {
+
+      "module": "new-module",
+
+      "only-module$": "new-module",
+
+      "module": path.resolve(__dirname, "app/third/module.js"),
+    },
+  },
+
+  performance: {
+    hints: "warning", // 枚举
+    maxAssetSize: 200000, // 整数类型（以字节为单位）
+    maxEntrypointSize: 400000, // 整数类型（以字节为单位）
+    assetFilter: function(assetFilename) {
+      // 提供资源文件名的断言函数
+      return assetFilename.endsWith('.css') || assetFilename.endsWith('.js');
+    }
+  },
+
+  devtool: "source-map", // enum
+  // 通过在浏览器调试工具(browser devtools)中添加元信息(meta info)增强调试
+  // 牺牲了构建速度的 `source-map' 是最详细的。
+
+  context: __dirname, // string（绝对路径！）
+  // webpack 的主目录
+  // entry 和 module.rules.loader 选项
+  // 相对于此目录解析
+
+  target: "web", // 枚举
+  // 包(bundle)应该运行的环境
+  // 更改 块加载行为(chunk loading behavior) 和 可用模块(available module)
+
+  externals: ["react", /^@angular\//],
+  // 不要遵循/打包这些模块，而是在运行时从环境中请求他们
+
+  stats: "errors-only",
+  // 精确控制要显示的 bundle 信息
+
+  devServer: {
+    proxy: { // proxy URLs to backend development server
+      '/api': 'http://localhost:3000'
+    },
+    contentBase: path.join(__dirname, 'example'), // boolean | string | array, static file location
+    compress: true, // enable gzip compression
+    historyApiFallback: true, // true for index.html upon 404, object for multiple paths
+    hot: true, // hot module replacement. Depends on HotModuleReplacementPlugin
+    https: false, // true for self-signed, object for cert authority
+    noInfo: true, // only errors & warns on hot reload
+    // ...
+  },
+
+  plugins: [
+    // ...
+  ],
+  // 附加插件列表
+
+
+  /* 高级配置（点击展示） */
+}
